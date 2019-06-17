9c5acc8f2fafe5029de13181013d8e42d66563cb
Erick Lopez <erickmis96@comunidad.unam.mx>
2019-06-17 15:16:07 -0500

    se agrego la pagina de ejemplo con materialize

---
+++ Web/js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+});
